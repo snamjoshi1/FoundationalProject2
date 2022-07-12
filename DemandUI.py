e723ac0a3bd02e517c838cd80aa615c0d4f45459
Shreyas Namjoshi <snamjoshi@altimetrik.com>
2022-07-12 17:38:16 +0530

    Major Version 2.0

--- DemandUI.py
+++ DemandUI.py
@@ -35,15 +35,26 @@ def ur_model():
 def demand_model():
     demandModel=load_model("DemandForecastingModel")
     return demandModel
+  
     
-def app_layout():
+def term():
     st.title("Demand Forecasting")
-    FutureMonths=st.number_input('Months',min_value=1,max_value=24,value=5)
-    return FutureMonths
+    option = st.selectbox('Select Term for Forecast',('Short Term', 'Long Term'))
+    return option
+    
+def app_layout(option):
+    if option=='Short Term':
+        FutureMonths=st.number_input('Months',min_value=1,max_value=12,value=3)
+        return FutureMonths
+    elif option=='Long Term':
+        FutureMonths=st.number_input('Months',min_value=13,max_value=24,value=13)
+        return FutureMonths
+        
 def fig(df,date,y):
     newDF=df
     newDF['Date']=date
     fig = px.line(newDF, x='Date', y=[y], template = 'plotly_dark',width=500,height=300)
+    newDF.drop(['Date'],axis=1,inplace=True)
     return fig
     
     
@@ -51,16 +62,18 @@ def fig(df,date,y):
 def predict():
     start='2022-07-01'
     starttime=datetime.strptime(start,'%Y-%m-%d')
-    future_date = starttime + relativedelta(months=app_layout())
+    option=term()
+    fmonths=app_layout(option)
+    future_date = starttime + relativedelta(months=fmonths-1)
     end=future_date.strftime('%Y-%m-%d')
     future_dates = pd.date_range(start = start, end = end, freq = 'MS')
     future_df = pd.DataFrame()
+    future_df['Series'] = np.arange(151,(151+len(future_dates)))
     future_df['Month'] = [i.month for i in future_dates]
     future_df['Year'] = [i.year for i in future_dates]    
-    future_df['Series'] = np.arange(79,(79+len(future_dates)))
     model1=temp_model()
     temp_future = predict_model(model1, data=future_df)
-    temp_future.columns=['Month','Year','Series','ForecastedTemperature']
+    temp_future.columns=['Series','Month','Year','ForecastedTemperature']
     
     st.subheader("Temperature Forecasting")
     col1,col2=st.columns(2)
@@ -72,7 +85,7 @@ def predict():
     col1,col2=st.columns(2)
     model2=humidity_model()
     humidity_future = predict_model(model2, data=future_df)
-    humidity_future.columns=['Month','Year','Series','ForecastedHumidity']
+    humidity_future.columns=['Series','Month','Year','ForecastedHumidity']
     col1.dataframe(humidity_future)
     image=fig(humidity_future,future_dates,'ForecastedHumidity')
     col2.plotly_chart(image)
@@ -81,7 +94,7 @@ def predict():
     col1,col2=st.columns(2)
     model3=rainfall_model()
     rainfall_future = predict_model(model3, data=future_df)
-    rainfall_future.columns=['Month','Year','Series','ForecastedRainfall']
+    rainfall_future.columns=['Series','Month','Year','ForecastedRainfall']
     col1.dataframe(rainfall_future)
     image=fig(rainfall_future,future_dates,'ForecastedRainfall')
     col2.plotly_chart(image)
@@ -90,7 +103,7 @@ def predict():
     col1,col2=st.columns(2)
     model4=windspeed_model()
     windspeed_future = predict_model(model4, data=future_df)
-    windspeed_future.columns=['Month','Year','Series','ForecastedWindSpeed']
+    windspeed_future.columns=['Series','Month','Year','ForecastedWindSpeed']
     col1.dataframe(windspeed_future)
     image=fig(windspeed_future,future_dates,'ForecastedWindSpeed')
     col2.plotly_chart(image)
@@ -99,7 +112,7 @@ def predict():
     col1,col2=st.columns(2)
     model5=ur_model()
     ur_future = predict_model(model5, data=future_df)
-    ur_future.columns=['Month','Year','Series','ForecastedUnemploymentRate']
+    ur_future.columns=['Series','Month','Year','ForecastedUnemploymentRate']
     col1.dataframe(ur_future)
     image=fig(ur_future,future_dates,'ForecastedUnemploymentRate')
     col2.plotly_chart(image)  
@@ -107,16 +120,17 @@ def predict():
     st.subheader("Demand Forecasting")
     col1,col2=st.columns(2)
     model6=demand_model()
-    newDF=[temp_future,humidity_future,windspeed_future,rainfall_future,ur_future]
-    final_df = reduce(lambda  left,right: pd.merge(left,right,on=['Series','Year','Month'],
-                                            how='inner'), newDF)
+    newDF1=[temp_future,humidity_future,windspeed_future,rainfall_future,ur_future]
+    final_df = reduce(lambda  left,right: pd.merge(left,right,on=['Series','Month','Year'],
+                                            how='inner'), newDF1)
     
-    predict_df=final_df[['ForecastedTemperature','ForecastedHumidity','ForecastedWindSpeed','ForecastedRainfall','ForecastedUnemploymentRate','Month','Year','Series']]
-    predict_df.columns=['AvgTemp', 'AvgHumidity', 'AvgWindspeed', 'AvgRainfall','UnemploymentRate', 'Month', 'Year', 'Series']
+    
+    predict_df=final_df[['Series','Month', 'Year','ForecastedTemperature','ForecastedHumidity','ForecastedWindSpeed','ForecastedRainfall','ForecastedUnemploymentRate']]
+    predict_df.columns=[ 'Series','Month', 'Year','AvgTemp', 'AvgHumidity', 'AvgWindspeed', 'AvgRainfall','UnemploymentRate']
     #['AvgTemp', 'AvgHumidity', 'AvgWindspeed', 'AvgRainfall','UnemploymentRate', 'Month', 'Year', 'Series']
     
     demand_future = predict_model(model6, data=predict_df)
-    demand_future.columns=['AvgTemp', 'AvgHumidity', 'AvgWindspeed', 'AvgRainfall','UnemploymentRate', 'Month', 'Year', 'Series','ForecastedDemand']
+    demand_future.columns=['Series','Month', 'Year','AvgTemp', 'AvgHumidity', 'AvgWindspeed', 'AvgRainfall','UnemploymentRate','ForecastedDemand']
     col1.dataframe(demand_future)
     image=fig(demand_future,future_dates,'ForecastedDemand')
     col2.plotly_chart(image)
